c7ee68cbe58e00a2bfe34d108e581acfc81d18b3
Rhys Bowen Harries <rhys@optimiseweb.co.uk>
2020-12-01 09:53:06 +0000

    date format perfect

--- src/components/LaunchList.js
+++ src/components/LaunchList.js
@@ -12,14 +12,21 @@ class LaunchList extends React.Component {
 
   dateHelper(unixTimestamp) {
     const milliseconds = unixTimestamp * 1000
-    const dateObject = new Date(milliseconds)
-    return dateObject.toLocaleString(
-      "en-GB",
-      {
-        day: "numeric",
-        month: "short",
-        year: "numeric"
-      })
+    const nth = function (d) {
+      if (d > 3 && d < 21) return 'th';
+      switch (d % 10) {
+        case 1: return "st";
+        case 2: return "nd";
+        case 3: return "rd";
+        default: return "th";
+      }
+    }
+
+    const unformatedDate = new Date(milliseconds);
+    const date = unformatedDate.getDate();
+    const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][unformatedDate.getMonth()];
+
+    return `${date}${nth(date)} ${month} ${unformatedDate.getFullYear()}`;
   }
 
   renderList() {
