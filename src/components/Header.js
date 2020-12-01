2c58153e02511c2fa7f08a2de6b454df411ffb28
Rhys Bowen Harries <rhys@optimiseweb.co.uk>
2020-12-01 16:58:48 +0000

    button css complte

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
+
 import spaceXLogo from '../assets/spacex-logo.png'
+import reloadIcon from "../assets/icon/refresh.png"
 import './Header.scss'
 
 const Header = () => {
@@ -7,6 +9,11 @@ const Header = () => {
     <div className="logo-container">
       <img src={spaceXLogo} alt="SpaceX Logo" className="logo" />
       <p className="logo-text">LAUNCHES</p>
+      <button className="reload-button">
+        <p className="button-content"> Reload Data </p>
+        <img src={reloadIcon} alt="Reload Icon" className="icon" />
+
+      </button>
     </div>
   )
 }
